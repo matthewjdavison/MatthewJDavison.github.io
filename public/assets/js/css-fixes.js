559efe640cf3fabfca98afb3a2bd8ad7b4bc99ee
matthewjdavison <matthewjdavison1+personalgithub@outlook.com>
2021-07-07 15:01:38 +0100

    Removed Public from Local

--- public/assets/js/css-fixes.js
+++
@@ -1,7 +0,0 @@
-// viewheight unit for mobile bug fix:
-function defineVH(){
-    let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-defineVH();
-window.addEventListener('resize', defineVH());
