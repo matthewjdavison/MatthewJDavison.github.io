220ac1129828cc7aad28e51c061cdc9cbea57968
matthewjdavison <matthewjdavison1+personal@outlook.com>
2021-08-06 23:04:37 +0100

    Fix some minor bugs. Removed some old code I found

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,16 +1,16 @@
 const portfolioItems = document.querySelector(".projects");
 let anyModalOpen = false;
 function openModal(e, linkClass){
+    e.preventDefault();
 if (!anyModalOpen) {
         const modalToggle = e.target.closest(linkClass);
         if (modalToggle) {
+
             const modal = modalToggle.parentElement.nextElementSibling;
-            console.log(modal);
+            document.body.style.overflowY = "hidden";    
             modal.classList.add('open');
-            document.body.style.overflowY= "hidden";    
             anyModalOpen = true;
             const close = modal.children[0];
-            console.log(close);
             close.addEventListener("click", _ => {
                 modal.classList.remove('open');
                 anyModalOpen = false;
@@ -32,8 +32,7 @@ for (let i = 0; i < projects.length; i++){
             console.log(e.target);
             if (!anyModalOpen) {                
                 const modal = e.target.querySelector('.portfolio-modal');
-                if (modal) {
-                    
+                if (modal) {                    
                     console.log(modal);
                     modal.classList.add('open');
                     document.body.style.overflowY= "hidden";    
