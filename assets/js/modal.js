589a1e1a4082b76cd63d5f94a7b5118d9bba678d
matthewjdavison <matthewjdavison1+personal@outlook.com>
2021-08-06 21:13:48 +0100

    fix git

---
+++ assets/js/modal.js
@@ -0,0 +1,52 @@
+const portfolioItems = document.querySelector(".projects");
+let anyModalOpen = false;
+function openModal(e, linkClass){
+if (!anyModalOpen) {
+        const modalToggle = e.target.closest(linkClass);
+        if (modalToggle) {
+            const modal = modalToggle.parentElement.nextElementSibling;
+            console.log(modal);
+            modal.classList.add('open');
+            document.body.style.overflowY= "hidden";    
+            anyModalOpen = true;
+            const close = modal.children[0];
+            console.log(close);
+            close.addEventListener("click", _ => {
+                modal.classList.remove('open');
+                anyModalOpen = false;
+                document.body.style.overflowY= "scroll";    
+            })
+        }
+    }   
+}
+
+portfolioItems.addEventListener("click", (e) => openModal(e, '.project__link'));
+// ====================================================== //
+// ================ Portfolio Interactive =============== //
+// ====================================================== //
+
+const projects = document.querySelector('.projects').children;
+for (let i = 0; i < projects.length; i++){
+    projects[i].onkeydown = (e) => {
+        if(e.keyCode === 13){
+            console.log(e.target);
+            if (!anyModalOpen) {                
+                const modal = e.target.querySelector('.portfolio-modal');
+                if (modal) {
+                    
+                    console.log(modal);
+                    modal.classList.add('open');
+                    document.body.style.overflowY= "hidden";    
+                    anyModalOpen = true;
+                    const close = modal.children[0];
+                    console.log(close);
+                    close.addEventListener("click", _ => {
+                        modal.classList.remove('open');
+                        anyModalOpen = false;
+                        document.body.style.overflowY= "scroll";    
+                    })
+                }
+            }             
+        }
+    };
+}
